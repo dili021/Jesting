42855f5d06754aa01a572af799a9f0171e36e2ae
stefan dili <stefan.dili.021@gmail.com>
2020-09-11 10:12:42 +0200

    analyze test done

--- js/Analyze.js
+++ js/Analyze.js
@@ -10,5 +10,4 @@ const analyze = arr => {
     length,
   };
 };
-
 export default analyze;
---
+++ js/spec/analyze.test.js
@@ -0,0 +1,15 @@
+import analyze from '../analyze';
+
+const tester = analyze([1, 2, 3, 4, 5]);
+
+test('It should return a resulting object', () => {
+  expect(typeof tester).toBe('object');
+});
+
+test('The object should have 4 properties', () => {
+  expect(Object.keys(tester).length).toEqual(4);
+});
+
+test('The properties should be average, min, max, and length', () => {
+  expect(Object.keys(tester)).toContain('average', 'min', 'max', 'length');
+});
